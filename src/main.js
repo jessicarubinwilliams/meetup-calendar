2e0bac645b387f158377ba9fec4af4c8904e62c1
shanole <73411129+shanole@users.noreply.github.com>
2021-03-30 20:23:44 -0700

    trying out fs?

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+// import fs from 'fs';
+
+// // const fs = require('fs');
+
+// let data = fs.readFileSync('./course-catalog.json');
+// let readData = JSON.parse(data);
+// console.log(readData);
