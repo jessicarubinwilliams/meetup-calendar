cd16cd7bcbc28d85d09bba7b646395b9f49a7675
JuanHasbunZem <zemenarwow@gmail.com>
2021-03-31 20:29:50 -0700

    fix title name, remove un-needed dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const fs = require('fs');
 
 module.exports = {
   entry: './src/main.js',
